5a218d7e584b1e8f42d50532562ea8260a5134cb
Brian Windrim <bwindrim@gmail.com>
2022-04-10 17:29:39 +0100

    Updates to piwatcher servce: - run service as root, so that it can initiate shutdown - use shutdown now instead of shutdown +15 - have the script count down the minutes until shutdown   (avoids problems if the current time changes)

--- piwatcher.sh
+++ piwatcher.sh
@@ -3,12 +3,20 @@
 /usr/local/bin/piwatcher watch 180  # set 3-minute watchdog timeout
 /usr/local/bin/piwatcher wake 14400 # set to wake after 4 hours
 
-# Ensure that we'll shutdown even if the battmon service doesn't start
-/sbin/shutdown +15 "Backstop 15-minute shutdown from piwatcher.service"
+counter=15 # 15-minute countdown until shutdown
 
 while true;
 do
+    echo "counter = " $counter
     /usr/local/bin/piwatcher status # >> /dev/null
+
+    if [ $counter -le 0 ]
+    then
+	# Ensure that we'll shutdown even if the battmon service doesn't start
+	/sbin/shutdown now "Backstop 15-minute shutdown from piwatcher.service"
+    fi
+
+    ((counter--))
     sleep 60
 done
 
