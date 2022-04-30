fd1495c2d09c597fe33036e3b24e4e42cd05f989
Brian Windrim <bwindrim@gmail.com>
2022-04-30 12:33:44 +0100

    BirdBox3 versions of raspistill.sh and raspivid.sh

--- raspistill.sh
+++ raspistill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=$(date +"BB1_%FT%H-%M-%S.jpg")
+FILE=$(date +"BB3_%FT%H-%M-%S.jpg")
 
-/usr/bin/raspistill -awb off -awbg '1.0,1.0' -n -t 500 -w 640 -h 480 -q 75 -o - | tee /mnt/capture/$FILE
+/usr/bin/raspistill -n -t 500 -rot 180 -w 640 -h 480 -q 75 -o - | tee /tmp/$FILE
 
