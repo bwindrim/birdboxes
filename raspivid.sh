854954befa11f9c7e616575d9ed2e2c9ac8a324f
Brian Windrim <bwindrim@gmail.com>
2023-03-02 19:51:14 +0000

    Updated capture scripts for new scheme

--- raspivid.sh
+++ raspivid.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 FILE=$(date +"BB1_%FT%H-%M-%S.h264")
+LOCAL_PATH=/mnt/local/video
 
-/usr/bin/raspivid -awb off -awbg '1.0,1.0' -t 0 -n -w 640 -h 480 -o - | tee --output-error=exit /mnt/capture/$FILE
+/usr/bin/raspivid -awb off -awbg '1.0,1.0' -t 0 -n -w 640 -h 480 -o - | tee --output-error=exit $LOCAL_PATH/$FILE
+chmod a-w $LOCAL_PATH/$FILE
 
