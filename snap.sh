51f3807b1e06dd1b61c8b62acac78afdaffdf7ee
Brian Windrim <bwindrim@gmail.com>
2023-03-02 19:49:51 +0000

    Adding capture scripts to bb2 for first time

---
+++ snap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE=$(date +"BB2_%FT%H-%M-%S.jpg")
+LOCAL_PATH=/mnt/local/timelapse
+REMOTE_PATH=/mnt/remote/birdbox2/timelapse
+
+/usr/bin/raspistill -awb off -awbg '1.0,1.0' -n -t 500 -w 640 -h 480 -q 75 -o $LOCAL_PATH/$FILE
+chmod a-w $LOCAL_PATH/$FILE
+
+if [ -d "$REMOTE_PATH" ]; then
+    cp -p $LOCAL_PATH/$FILE $REMOTE_PATH/$FILE
+fi
+
