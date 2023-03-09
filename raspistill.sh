a2ef091ae0cd0a1b5760291e947b9d5bc1b01fc1
Brian Windrim <bwindrim@gmail.com>
2023-03-09 13:29:19 +0000

    Changes for new testbed setup

--- raspistill.sh
+++ raspistill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=$(date +"BB1_%FT%H-%M-%S.jpg")
+FILE=$(date +"TB_%FT%H-%M-%S.jpg")
 LOCAL_PATH=/mnt/local/photo
 
 /usr/bin/raspistill -awb off -awbg '1.0,1.0' -n -t 500 -w 640 -h 480 -q 75 -o - | tee $LOCAL_PATH/$FILE
--- snap.sh
+++ snap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-FILE=$(date +"BB1_%FT%H-%M-%S.jpg")
+FILE=$(date +"TB_%FT%H-%M-%S.jpg")
 LOCAL_PATH=/mnt/local/timelapse
-REMOTE_PATH=/mnt/remote/birdbox1/timelapse
+REMOTE_PATH=/mnt/remote/testbed/timelapse
 
 /usr/bin/raspistill -awb off -awbg '1.0,1.0' -n -t 500 -w 640 -h 480 -q 75 -o $LOCAL_PATH/$FILE
 chmod a-w $LOCAL_PATH/$FILE
