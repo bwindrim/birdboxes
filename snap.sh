dd44bebca0bd83a7fec11755c6f646acf8e0f74c
Brian Windrim <bwindrim@gmail.com>
2023-03-25 12:22:47 +0000

    Changed PLATFORM to BirdBox3, and removed old code

--- snap.sh
+++ snap.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-FILE=$(date +"TB_%FT%H-%M-%S.jpg")
-LOCAL_PATH=/mnt/local/timelapse
-REMOTE_PATH=/mnt/remote/testbed/timelapse
 DATETIME=$(date)
-
-#/usr/bin/raspistill -awb off -awbg '1.0,1.0' -n -t 500 -w 640 -h 480 -q 75 -o $LOCAL_PATH/$FILE
-#chmod a-w $LOCAL_PATH/$FILE
-
-#if [ -d "$REMOTE_PATH" ]; then
-#    cp -p $LOCAL_PATH/$FILE $REMOTE_PATH/$FILE
-#fi
-
-PLATFORM="TestBed"
+PLATFORM="BirdBox3"
 MESSAGE="${PLATFORM}: ${DATETIME}"
-#python3 /home/pi/birdboxes/msnap.py $PLATFORM $MESSAGE
 python3 /home/pi/birdboxes/msnap.py $PLATFORM "$MESSAGE"
 
 
