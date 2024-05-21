82bb4e7bfa644a8f7a7e620e9559dfc7adbebf55
Brian Windrim <bwindrim@gmail.com>
2024-05-21 09:36:02 +0100

    Switch LEDs on for photo, if enabled by GPIO6, then back to previous state

--- snap.sh
+++ snap.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-
+if [ ! -d /sys/class/gpio/gpio5 ] ; then
+	echo 5 >/sys/class/gpio/export
+fi
+if [ ! -d /sys/class/gpio/gpio6 ] ; then
+	echo 6 >/sys/class/gpio/export
+fi
+cat /sys/class/gpio/gpio5/value
+cat /sys/class/gpio/gpio6/value
+old=$(cat /sys/class/gpio/gpio5/value)
+new=$(cat /sys/class/gpio/gpio6/value)
+echo $new >/sys/class/gpio/gpio5/value
+cat /sys/class/gpio/gpio5/value
+cat /sys/class/gpio/gpio6/value
 DATETIME=$(date)
 PLATFORM=$(hostname)
 MESSAGE="${PLATFORM}: ${DATETIME}"
 python3 /home/pi/birdboxes/msnap.py $PLATFORM "$MESSAGE"
+echo $old >/sys/class/gpio/gpio5/value
+cat /sys/class/gpio/gpio5/value
+cat /sys/class/gpio/gpio6/value
+
 
 
