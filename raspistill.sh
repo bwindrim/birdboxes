3e3999cc1aadb6981b9c2176e82dcf5e5872827f
Brian Windrim <bwindrim@gmail.com>
2024-05-22 21:29:50 +0100

    Switch on LEDs only when needed, using GPIO4

--- raspistill.sh
+++ raspistill.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ ! -d /sys/class/gpio/gpio5 ] ; then
+	echo 5 >/sys/class/gpio/export
+fi
+if [ ! -d /sys/class/gpio/gpio4 ] ; then
+	echo 4 >/sys/class/gpio/export
+fi
+old=$(cat /sys/class/gpio/gpio5/value)
+new=$(cat /sys/class/gpio/gpio4/value)
+echo $new >/sys/class/gpio/gpio5/value
 
 FILE=$(date +"BB1_%FT%H-%M-%S.jpg")
 LOCAL_PATH=/mnt/local/photo
@@ -6,3 +15,4 @@ LOCAL_PATH=/mnt/local/photo
 /usr/bin/raspistill -awb off -awbg '1.0,1.0' -n -t 500 -w 640 -h 480 -q 75 -o - | tee $LOCAL_PATH/$FILE
 chmod a-w $LOCAL_PATH/$FILE
 
+echo $old >/sys/class/gpio/gpio5/value
--- raspivid.sh
+++ raspivid.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ ! -d /sys/class/gpio/gpio5 ] ; then
+	echo 5 >/sys/class/gpio/export
+fi
+if [ ! -d /sys/class/gpio/gpio4 ] ; then
+	echo 4 >/sys/class/gpio/export
+fi
+old=$(cat /sys/class/gpio/gpio5/value)
+new=$(cat /sys/class/gpio/gpio4/value)
+echo $new >/sys/class/gpio/gpio5/value
 
 FILE=$(date +"BB1_%FT%H-%M-%S.h264")
 LOCAL_PATH=/mnt/local/video
@@ -6,3 +15,4 @@ LOCAL_PATH=/mnt/local/video
 /usr/bin/raspivid -awb off -awbg '1.0,1.0' -t 0 -n -w 640 -h 480 -o - | tee --output-error=exit $LOCAL_PATH/$FILE
 chmod a-w $LOCAL_PATH/$FILE
 
+echo $old >/sys/class/gpio/gpio5/value
