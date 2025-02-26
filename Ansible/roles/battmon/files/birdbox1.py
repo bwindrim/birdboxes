3c1d9fffb6efd96b0543cd8a640290ba3824f345
Brian Windrim <bwindrim@gmail.com>
2025-02-26 12:26:08 +0000

    Timing and message adjustments, sent to tree

--- Ansible/roles/battmon/files/birdbox1.py
+++ Ansible/roles/battmon/files/birdbox1.py
@@ -90,7 +90,7 @@ def getBatteryLevel(numReads=20):
 def evaluate(now, level):
     "Decide how long to stay up and to sleep, based on current time-of-day and battery level"
     if now < minutes(0,5,30): # It's after midnight but before 8:30, power off until 9:00 today
-        stay_up = 0
+        stay_up = 10
         wake_time = minutes(0,8,0)
         message = "Night-time immediate shutdown"
         return (stay_up, wake_time, message) # early out
@@ -99,12 +99,12 @@ def evaluate(now, level):
         wake_time = minutes(0,8,0)
         message = "Early morning 1-hour shutdown"
         return (stay_up, wake_time, message) # early out
-    elif level >= 100: # 4 battery bars, stay up for 3 hours then power off for 3 hours
-        stay_up = 180
+    elif level >= 100: # 4 battery bars, stay up for 2 hours then power off for 3 hours
+        stay_up = 120
         wake_time = now + 360
         message = "Scheduled two-hour shutdown"
-    elif level >= 85: # 3-4 battery bars, stay up for 2 hours then power off for 4 hours
-        stay_up = 120
+    elif level >= 85: # 3-4 battery bars, stay up for 1.5 hours then power off for 4 hours
+        stay_up = 90
         wake_time = now + 360
         message = "Scheduled one-hour shutdown"
     elif level >= 75: # 3 battery bars, stay up for 1 hour then power off for 5 hours
@@ -115,12 +115,12 @@ def evaluate(now, level):
         stay_up = 30
         wake_time = now + 720
         message = "Scheduled half-hour shutdown"
-    elif level >= 50: # 2 battery bars, stay up for 15 minutes then power off until 8:00 tomorrow
-        stay_up = 15
+    elif level >= 50: # 2 battery bars, stay up for 20 minutes then power off until 8:00 tomorrow
+        stay_up = 20
         wake_time = minutes(1,8,0)
         message = "Scheduled 15-minute shutdown"
     else: # Battery critical, power off immediately until 12:00 tomorrow
-        stay_up = 0
+        stay_up = 10
         wake_time = minutes(1,12,0)
         message = "Emergency shutdown"
     wake_time = wake_time // 15 * 15 # Round wake time down to nearest 15 minutes
@@ -206,7 +206,7 @@ try:
     print("stay-up duration =", stay_up, "wake-up time =", wake_time)
     client.publish("birdboxes/birdbox1/initial_stay_up", stay_up, qos=1, retain=True)
     client.publish("birdboxes/birdbox1/wake_time", timestr(wake_time), qos=1, retain=True)
-    ntfy(f'BirdBox1 up: batt {level}%, stay up {stay_up} mins')
+    ntfy(f'BirdBox1 up at {timestr(now)}, for {stay_up} mins: batt {level}%')
     # Main watchdog wakeup loop
     while stay_up > 0:
         # Sleep for one minute
@@ -237,7 +237,7 @@ try:
     print("Shutting down, wake time is", timestr(wake_time))
     client.publish("birdboxes/birdbox1/shutdown_time", time.asctime(), qos=1, retain=True)
     client.publish("birdboxes/birdbox1/wake_time", timestr(wake_time), qos=1, retain=True)
-    ntfy(f'BirdBox1 going down until {timestr(wake_time)}')
+    ntfy(f'BirdBox1 down at {timestr(now)}, until {timestr(wake_time)}: batt {level}%, {message}')
     if exists("/tmp/noshutdown"): # if shutdown is to be blocked
         print("Shutdown blocked by /tmp/noshutdown, deferring by one hour")
         system_shutdown(message, when="+60")
